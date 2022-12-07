ea32c05d2a87c58e1161e54cc56c3374a319dc41
ginohmk <michotall95@gmail.com>
2022-12-06 21:58:53 -0800

    addition

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add(a,b):
+    """My addition fuction
+
+    Args:
+     a: first integer
+     b: secod integer
+
+    Returns:
+     The return value. a + b
+    """
+    return (a + b)
